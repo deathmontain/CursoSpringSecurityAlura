aa7ad7acbf46fbcc9a41fd90e5ffa0c6c6568a85
Jônatas Santos <jonatas.santos@zup.com.br>
2019-08-09 15:24:17 -0300

    Adicionado controle de paginação ao controller lista

--- src/main/java/br/com/jonatas/forum/controller/Dto/TopicoDto.java
+++ src/main/java/br/com/jonatas/forum/controller/Dto/TopicoDto.java
@@ -2,6 +2,7 @@ package br.com.jonatas.forum.controller.Dto;
 
 import br.com.jonatas.forum.model.Topico;
 import lombok.Data;
+import org.springframework.data.domain.Page;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -23,7 +24,7 @@ public class TopicoDto {
         this.dataCriacao = topico.getDataCriacao();
     }
 
-    public static List<TopicoDto> converter(List<Topico> topicos) {
-        return topicos.stream().map(TopicoDto::new).collect(Collectors.toList());
+    public static Page<TopicoDto> converter(Page<Topico> topicos) {
+        return topicos.map(TopicoDto::new);
     }
 }
--- src/main/java/br/com/jonatas/forum/repository/TopicoRepository.java
+++ src/main/java/br/com/jonatas/forum/repository/TopicoRepository.java
@@ -1,11 +1,11 @@
 package br.com.jonatas.forum.repository;
 
 import br.com.jonatas.forum.model.Topico;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
 
 public interface TopicoRepository extends JpaRepository <Topico, Long> {
 
-    List<Topico> findByCurso_Nome(String nomeCurso);
+    Page<Topico> findByCurso_Nome(String nomeCurso, Pageable paginacao);
 }
