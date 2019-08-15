5b503a1342519f78cd2ae4e38157a036c8158cf8
Jônatas Santos <jonatas.santos@aedu.com>
2019-08-15 15:08:21 -0300

    Configurado a autenticação do usuário

---
+++ src/main/java/br/com/jonatas/forum/repository/UsuarioRepository.java
@@ -0,0 +1,10 @@
+package br.com.jonatas.forum.repository;
+
+import br.com.jonatas.forum.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByEmail(String email);
+}
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$EwYGtMO.bM3FYE3vYd9t3u.bkNOzxbSO3lJtF1cYnT8FwmLGdL.ZC');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
